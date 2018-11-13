3664bb592af4d41c634ba510b11cbd840e3a8f8c
Amrita <theamritanair@gmail.com>
2018-11-13 21:00:48 +0530

    Made UI fixes, and returns the JSON string

--- app/src/main/java/com/example/android/findthemeaning/DisplayActivity.java
+++ app/src/main/java/com/example/android/findthemeaning/DisplayActivity.java
@@ -2,12 +2,27 @@ package com.example.android.findthemeaning;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class DisplayActivity extends AppCompatActivity {
+
+    @BindView(R.id.displayText)
+    TextView display;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display);
+
+        ButterKnife.bind(this);
+
+        Bundle bun = getIntent().getExtras();
+
+        display.setText(bun.getString("key"));
+
+
     }
 }
--- app/src/main/java/com/example/android/findthemeaning/MainActivity.java
+++ app/src/main/java/com/example/android/findthemeaning/MainActivity.java
@@ -1,13 +1,111 @@
 package com.example.android.findthemeaning;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import javax.net.ssl.HttpsURLConnection;
+import android.os.AsyncTask;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.searchText)
+    EditText searchArea;
+
+    @BindView(R.id.searchButton)
+    Button searchButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                new CallbackTask().execute(dictionaryEntries());
+            }
+        });
+
+
+
+
     }
+
+    private String dictionaryEntries() {
+        final String language = "en";
+        final String word = searchArea.getText().toString();
+        final String word_id = word.toLowerCase(); //word id is case sensitive and lowercase is required
+        return "https://od-api.oxforddictionaries.com:443/api/v1/entries/" + language + "/" + word_id;
+    }
+
+
+    private class CallbackTask extends AsyncTask<String, Integer, String> {
+
+        @Override
+        protected String doInBackground(String... params) {
+
+            //TODO: replace with your own app id and app key
+            final String app_id = "fb3fc25c";
+            final String app_key = "92e3e400116f3a4f74c580496c3fe82a";
+            try {
+                URL url = new URL(params[0]);
+                HttpsURLConnection urlConnection = (HttpsURLConnection) url.openConnection();
+                urlConnection.setRequestProperty("Accept","application/json");
+                urlConnection.setRequestProperty("app_id",app_id);
+                urlConnection.setRequestProperty("app_key",app_key);
+
+                // read the output from the server
+                BufferedReader reader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+                StringBuilder stringBuilder = new StringBuilder();
+
+                String line = null;
+                while ((line = reader.readLine()) != null) {
+                    stringBuilder.append(line + "\n");
+                }
+
+                return stringBuilder.toString();
+
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+                return e.toString();
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+
+
+            try {
+                JSONObject json = new JSONObject(result);
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            Intent intent = new Intent(MainActivity.this, DisplayActivity.class);
+            intent.putExtra("key",result);
+            startActivity(intent);
+
+            System.out.println(result);
+        }
+    }
+
 }
